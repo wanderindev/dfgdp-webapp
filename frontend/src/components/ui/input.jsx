f1e39003d83ca62d76f2036971ecf42773b1c7b2
Javier Feliu <javier@monetizemore.com>
2024-12-20 10:37:31 -0500

    Include new UI components

--- frontend/src/components/ui/input.jsx
+++ frontend/src/components/ui/input.jsx
@@ -1,3 +1,6 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
     <input
@@ -13,4 +16,4 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
 })
 Input.displayName = "Input"
 
-export { Label, Input }
+export { Input }
--- frontend/src/components/ui/label.jsx
+++ frontend/src/components/ui/label.jsx
@@ -11,3 +11,5 @@ const Label = React.forwardRef(({ className, ...props }, ref) => (
   <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
 ))
 Label.displayName = LabelPrimitive.Root.displayName
+
+export { Label, labelVariants }
