d1a68c3a1769047ee2e3b8608323bf6d6e468cea
Javier Feliu <javier@monetizemore.com>
2025-01-30 16:24:00 -0500

    Create RecordStatus component

--- frontend/src/components/shared/RecordStatus.jsx
+++ frontend/src/components/shared/RecordStatus.jsx
@@ -1,8 +1,9 @@
 import {Check, X} from "lucide-react";
 import React from "react";
 
-export const SuggestionStatus = ({ status }) => {
-  switch (status) {
+
+export const RecordStatus = ({ value }) => {
+  switch (value) {
     case 'APPROVED':
       return (
         <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
@@ -15,6 +16,20 @@ export const SuggestionStatus = ({ status }) => {
         <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">
           <X className="w-4 h-4 mr-1" />
           Rejected
+        </span>
+      );
+    case 'ACTIVE':
+      return (
+        <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+          <Check className="w-4 h-4 mr-1" />
+          Active
+        </span>
+      );
+    case 'INACTIVE':
+      return (
+        <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">
+          <X className="w-4 h-4 mr-1" />
+          Inactive
         </span>
       );
     default:
